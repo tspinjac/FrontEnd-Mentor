bc44267fd3a4edd629e8ba8c5775544ee3a900c3
Tomislav Spinjaca <tomislav.spinjaca@t.ht.hr>
2020-04-24 22:31:10 +0200

    TBD Form validation info

--- 4_intro-component-with-signup-form-master/js/script.js
+++ 4_intro-component-with-signup-form-master/js/script.js
@@ -1,29 +1,76 @@
 /* Email Regex */
-function isValidEmail(emailText) {
+/* function isValidEmail(emailText) {
     var regExpr = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return regExpr.test(emailText);
-}
+} */
 
 
-
-
-$('#submit_btn').on('click', function() {
+/* $('#submit_btn').on('click', function() {
     console.log("OK");
     var emailText = $('#email').val();
 
     if (isValidEmail(emailText)) {
         $('.error_svg, .error_msg').css('display', 'none');
-        /*     document.getElementById("formica").reset(); */
 
     } else {
         $('.error_svg, .error_msg').css('display', 'block');
         $("#name, #lastName, #email, #password").addClass("input_error_border");
-
-        /* document.getElementById("nesto").reset(); */
-        /* $('#email').attr('placeholder', ''); */
-        /*      $('#email').focus(function() {
-                 $('.form-errorText').hide();
-             }); */
-
     }
+}); */
+
+
+/* JS part 'stolen' from https://github.com/Diarrah, all credits to her -ps: ty D for ideas anc concepts */
+
+function validateEmail(email) {
+    let validEmail = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return validEmail.test(String(email).toLowerCase());
+}
+
+let form = document.querySelector('#form');
+let fName = document.getElementById('firstName');
+let lName = document.getElementById('lastName');
+let email = document.getElementById('email');
+let password = document.getElementById('password');
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    checkInputs();
 });
+
+function checkInputs() {
+    let fNameVal = fName.value.trim();
+    let lNameVal = lName.value.trim();
+    let emailVal = email.value.trim();
+    let passwordVal = password.value.trim();
+
+    if (fNameVal === '') {
+        setError(fName, 'First Name cannot be empty');
+    } else { success(fName) };
+
+    if (lNameVal === '') {
+        setError(lName, 'Last Name cannot be empty');
+    } else { success(lName) };
+
+    if (emailVal === '') {
+        setError(email, 'Email cannot be empty')
+    } else if (!validateEmail(emailVal)) {
+        setError(email, 'Looks like this is not an email');
+    } else { success(email) }
+
+    if (passwordVal === '') {
+        setError(password, 'Password cannot be empty');
+    } else { success(password) };
+}
+
+function setError(message) {
+    let upozoronje = document.querySelector('.error_msg');
+    /*     formControl.className = 'form-control error'; */
+    console.log(upozoronje.value);
+    upozoronje.innerText = message;
+}
+
+function success(input) {
+    let upozoronje = input.querySelector('.error_msg');
+    /*     formControl.className = 'form-control error'; */
+    upozoronje.innerText = '';
+}
